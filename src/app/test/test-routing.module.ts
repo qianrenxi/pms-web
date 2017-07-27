6e27a1ca019788fbf8e36d274648ade1e4ccdf34
Tony <tonyeen@foxmail.com>
2017-07-28 00:26:32 +0800

    try routing right

--- src/app/test/test-routing.module.ts
+++ src/app/test/test-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TestComponent } from './test.component';
+import { TestHomeComponent } from './test-home/test-home.component';
 
 const routes: Routes = [
-  { path: '', component: TestComponent },
+  { path: '', component: TestComponent,
+  children: [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: TestHomeComponent},
+    {path: ':productId/task', loadChildren: 'app/test/test-task/test-task.module#TestTaskModule'}
+  ]},
 ];
 
 @NgModule({
